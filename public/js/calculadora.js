622ae5c352ea6101f417b989a4b0c37b9768c36c
JCPB2000 <jcpaguay1@espe.edu.ec>
2025-06-25 21:54:05 -0500

    Creación contenedor y div

--- public/js/calculadora.js
+++ public/js/calculadora.js
@@ -8,3 +8,27 @@ class CalculadoraBasica extends HTMLElement {
     // Estructura interna con Bootstrap
     this.shadowRoot.innerHTML = `
       <link rel="stylesheet" href="/public/lib/bootstrap/css/bootstrap.min.css">
+
+      <div class="container mt-4">
+        <div class="card shadow p-4">
+          <h4 class="text-center text-primary mb-3">Calculadora Básica</h4>
+
+          <div class="mb-3">
+            <label class="form-label">Primer número:</label>
+            <input type="number" id="num1" class="form-control" placeholder="Ej: 12">
+          </div>
+
+          <div class="mb-3">
+            <label class="form-label">Segundo número:</label>
+            <input type="number" id="num2" class="form-control" placeholder="Ej: 4">
+          </div>
+
+          <div class="mb-3">
+            <label class="form-label">Operación:</label>
+            <select id="operacion" class="form-select">
+              <option value="suma">Suma</option>
+              <option value="resta">Resta</option>
+              <option value="multiplicacion">Multiplicación</option>
+              <option value="division">División</option>
+            </select>
+          </div>
